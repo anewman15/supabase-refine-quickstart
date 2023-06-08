b029f9a8b9935b25a2f9d220b957a0430cafb198
Abdullah Numan <anewman15@hotmail.com>
2023-06-08 20:56:54 +0600

    feat(docs): update supabaseClient

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@refinedev/supabase";
 
-const SUPABASE_URL = "https://iwdfzvfqbtokqetmbmbp.supabase.co";
+const SUPABASE_URL = "https://rgwukmbwmpdsczyttzqg.supabase.co";
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnd3VrbWJ3bXBkc2N6eXR0enFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYyMjIzMTYsImV4cCI6MjAwMTc5ODMxNn0.jEZrHDhRLTx3VDc0_zfjO6SLS_-VQn7gofMp8VEGIew";
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   db: {
